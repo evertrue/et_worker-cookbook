0fa7661c3d5cad8284aeb202f44618e4e2fbf2d0
Jeff Byrnes <jeff@evertrue.com>
2013-10-02 16:49:54 -0400

    Use version 1.1.0 of `rds-s3-backups`, bump version to 1.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_worker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.1'
+version          '1.2.0'
 
-depends 'rds-s3-backups', '= 1.0.1'
+depends 'rds-s3-backups', '= 1.1.0'
 depends 'postfix'
 
 provides 'worker'
