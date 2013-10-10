9a230e965f65cb88dfe4f9a5a06d2ceb0f709a12
Eric Herot <eric.github@herot.com>
2013-10-10 18:01:11 -0400

    Revert to ~> 2.1 of postfix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,15 @@
+## 1.2.2:
+
+* Revert to ~> 2.1 of postfix
+
+## 1.2.1:
+
+* Jeff needs to update this
+
+## 1.1.1:
+
+* Bump rds-s3-backups to 1.0.2
+
 ## 1.1.0:
 
 * Add CHANGELOG
--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_worker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.1'
+version          '1.2.2'
 
 depends 'rds-s3-backups', '= 1.1.0'
-depends 'postfix', '~> 3.0.0'
+depends 'postfix', '~> 2.1'
 
 provides 'worker'
