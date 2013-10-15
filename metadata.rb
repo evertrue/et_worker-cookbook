389104e783c536e09d82e9f695e70c2da1ee1e20
Jeff Byrnes <jeff@evertrue.com>
2013-10-15 11:59:19 -0400

    Use v1.1.2 of rds-s3-backups cookbook, bump to 1.2.3
    Fixes a syntax error in the backup script

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_worker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.2'
+version          '1.2.3'
 
-depends 'rds-s3-backups', '= 1.1.0'
+depends 'rds-s3-backups', '= 1.1.2'
 depends 'postfix', '~> 2.1'
 
 provides 'worker'
