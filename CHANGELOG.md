ae4f911d6b440a33ec85431320d7bfa28c641986
Jeff Byrnes <jeff@evertrue.com>
2013-12-02 17:42:40 -0500

    Bump to 1.2.6, update CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,17 @@
+## 1.2.6
+
+* Tweaked Gemfile to use:
+    * Berkshelf 2.10.0
+    * Test Kitchen 1.0.0
+    * kitchen-vagrant 0.12.0
+* Use generic email address for maintainer
+
+## 1.2.5
+
+## 1.2.4
+
+## 1.2.3
+
 ## 1.2.2:
 
 * Revert to ~> 2.1 of postfix
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'et_worker'
 maintainer       'EverTrue, Inc.'
-maintainer_email 'eric.herot@evertrue.com'
+maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_worker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.5'
+version          '1.2.6'
 
 depends 'rds-s3-backups', '= 1.1.4'
 depends 'postfix', '~> 2.1'
