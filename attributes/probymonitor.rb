afc070ee8c061e90b0c125b9e12b0ce1bfb56c6f
Jeff Byrnes <jeff@evertrue.com>
2014-01-06 13:56:54 -0500

    Clean up as per Rubocop

--- attributes/probymonitor.rb
+++ attributes/probymonitor.rb
@@ -1,3 +1,3 @@
-default["probymonitor"]["api_key_databag"] = "secrets"
-default["probymonitor"]["api_key_databag_item"] = "api_keys"
-default['probymonitor']['task_id'] = ""
+default['probymonitor']['api_key_databag'] = 'secrets'
+default['probymonitor']['api_key_databag_item'] = 'api_keys'
+default['probymonitor']['task_id'] = ''
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,10 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "rds-s3-backups"
+include_recipe 'rds-s3-backups'
 
-node.set['postfix']['mail_relay_networks'] = node["ec2"]["network_interfaces_macs"][node["macaddress"].downcase]['vpc_ipv4_cidr_block'] if node["ec2"]
+node.set['postfix']['mail_relay_networks'] = node['ec2']['network_interfaces_macs'][node['macaddress'].downcase]['vpc_ipv4_cidr_block'] if node['ec2']
 
-include_recipe "postfix::server"
-include_recipe "et_worker::probymonitor"
-
+include_recipe 'postfix::server'
+include_recipe 'et_worker::probymonitor'
