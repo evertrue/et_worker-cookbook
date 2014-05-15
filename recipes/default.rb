29e0358baaaa385d173025025fceeef476dbbd3b
Jeff Byrnes <jeff@evertrue.com>
2014-05-15 17:12:03 -0400

    Set Berkshelf config attribute in recipe
    Follows Chef convention better, and allows for input of GitHub API token

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,6 +21,31 @@ if api_keys.nil?
   fail "Data bag secrets/api_keys is missing environment #{node.chef_environment}"
 end
 
+node.set['berkshelf_api']['config'] = {
+  home_path: node['berkshelf_api']['home'],
+  chef_server: {
+    type: 'chef_server',
+    options: {
+      url: 'https://api.opscode.com/organizations/evertrue',
+      client_name: 'berkshelf',
+      client_key: '/etc/berkshelf/api-server/client.pem'
+    }
+  },
+  opscode: {
+    type: 'opscode',
+    options: {
+      url: 'http://cookbooks.opscode.com/api/v1'
+    }
+  },
+  github: {
+    type: 'github',
+    options: {
+      organization: 'evertrue',
+      access_token: api_keys['github']
+    }
+  }
+}
+
 include_recipe 'postfix::server'
 include_recipe 'et_worker::probymonitor'
 include_recipe 'stale-node-checker'
