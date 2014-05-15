dacc5fe6e8e98a3f156bd07f28ce198d764b9e6e
Jeff Byrnes <jeff@evertrue.com>
2014-05-15 17:37:24 -0400

    Version bump to 1.4.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_worker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.4.0'
+version          '1.4.1'
 
 depends 'postfix', '~> 3.0.4'
 depends 'rds-s3-backups', '= 1.1.4'
