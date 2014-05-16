429a46619c89a06915ac935deda2925b825235f5
Jeff Byrnes <jeff@evertrue.com>
2014-05-16 13:28:18 -0400

    Fix syntax of array of hashes in Berkshelf API config

--- recipes/berkshelf_api.rb
+++ recipes/berkshelf_api.rb
@@ -19,7 +19,7 @@ end
 node.set['berkshelf_api']['config'] = {
   home_path: node['berkshelf_api']['home'],
   endpoints: [
-    chef_server: {
+    {
       type: 'chef_server',
       options: {
         url: 'https://api.opscode.com/organizations/evertrue',
@@ -27,13 +27,13 @@ node.set['berkshelf_api']['config'] = {
         client_key: '/etc/berkshelf/api-server/client.pem'
       }
     },
-    opscode: {
+    {
       type: 'opscode',
       options: {
         url: 'http://cookbooks.opscode.com/api/v1'
       }
     },
-    github: {
+    {
       type: 'github',
       options: {
         organization: 'evertrue',
