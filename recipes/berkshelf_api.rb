9e72dc5f6f5a15d3c6fe6fcc1dbb7e218dbc5f36
Jeff Byrnes <jeff@evertrue.com>
2014-05-16 13:10:02 -0400

    Fix configuration of Berkshelf API server endpoints

--- recipes/berkshelf_api.rb
+++ recipes/berkshelf_api.rb
@@ -18,27 +18,29 @@ end
 
 node.set['berkshelf_api']['config'] = {
   home_path: node['berkshelf_api']['home'],
-  chef_server: {
-    type: 'chef_server',
-    options: {
-      url: 'https://api.opscode.com/organizations/evertrue',
-      client_name: 'berkshelf',
-      client_key: '/etc/berkshelf/api-server/client.pem'
+  endpoints: [
+    chef_server: {
+      type: 'chef_server',
+      options: {
+        url: 'https://api.opscode.com/organizations/evertrue',
+        client_name: 'berkshelf',
+        client_key: '/etc/berkshelf/api-server/client.pem'
+      }
+    },
+    opscode: {
+      type: 'opscode',
+      options: {
+        url: 'http://cookbooks.opscode.com/api/v1'
+      }
+    },
+    github: {
+      type: 'github',
+      options: {
+        organization: 'evertrue',
+        access_token: api_keys['github']['berkshelf']
+      }
     }
-  },
-  opscode: {
-    type: 'opscode',
-    options: {
-      url: 'http://cookbooks.opscode.com/api/v1'
-    }
-  },
-  github: {
-    type: 'github',
-    options: {
-      organization: 'evertrue',
-      access_token: api_keys['github']['berkshelf']
-    }
-  }
+  ]
 }
 
 include_recipe 'berkshelf-api'
