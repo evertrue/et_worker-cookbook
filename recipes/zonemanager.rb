f4268a44c3abf462476ca323c41dd9be8c3aaed8
Eric Herot <eric.github@herot.com>
2014-06-27 14:54:19 -0400

    Implement and make use of "delete" action

--- recipes/zonemanager.rb
+++ recipes/zonemanager.rb
@@ -10,7 +10,7 @@ node['route53']['zones'].each do |zone|
   z_data = EtWorker::ZoneManager.new(data_bag_data[zone], creds)
 
   z_data.updated.each_with_index do |record, i|
-    route53_record "#{zone}_#{i}_#{record['name']}_#{record['type']}" do
+    route53_record "#{zone}_#{i}_#{record['name']}_#{record['type']}_create" do
       name                  record['name']
       value                 record['value']
       type                  record['type']
@@ -18,6 +18,17 @@ node['route53']['zones'].each do |zone|
       zone_id               data_bag_data[zone]['id']
       aws_access_key_id     creds['access_key_id']
       aws_secret_access_key creds['secret_access_key']
+    end
+  end
+
+  z_data.removed.each_with_index do |record, i|
+    route53_record "#{zone}_#{i}_#{record['name']}_#{record['type']}_delete" do
+      name                  record['name']
+      type                  record['type']
+      zone_id               data_bag_data[zone]['id']
+      aws_access_key_id     creds['access_key_id']
+      aws_secret_access_key creds['secret_access_key']
+      action               :delete
     end
   end
 end
