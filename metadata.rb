bb55eac933628448c49ae0448406f831150f6de7
Jeff Byrnes <jeff@evertrue.com>
2014-10-27 16:14:53 -0400

    Use published, community version of route53
    Best to not get confused with using our fork when all of our changes
    have been merged upstream.
    
    route53 v0.4.0 will be yanked once this is shipped

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ depends 'postfix', '~> 3.0.4'
 depends 'rds-s3-backups', '~> 1.2'
 depends 'stale-node-checker', '= 1.0.2'
 depends 'berkshelf-api-server', '= 1.4.0'
-depends 'route53', '= 0.4.0'
+depends 'route53', '= 0.3.6'
 depends 'ssh_known_hosts'
 
 provides 'worker'
