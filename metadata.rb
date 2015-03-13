e148efe06fb9d74e8f34184a4f69e09d088d8edf
Eric Herot <eric.github@herot.com>
2015-03-13 11:27:51 -0400

    Relax postfix version constraint

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,12 @@ description      'Installs/Configures et_worker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.0.1'
 
-depends 'postfix', '~> 3.0.4'
+depends 'postfix', '~> 3.1'
 depends 'rds-s3-backups', '~> 1.2'
 depends 'stale-node-checker', '= 1.0.2'
 depends 'berkshelf-api-server', '= 2.1.0'
 depends 'route53', '= 0.3.6'
 depends 'ssh_known_hosts'
+depends 'docker-registry', '~> 0.0'
 
 provides 'worker'
