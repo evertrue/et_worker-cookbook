a57b54612495547b4121aa3a8adae64a2d0fafe8
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-02-01 15:15:00 -0500

    Update stubbed command for Berkshelf v2.1.1
    Again, some drift of code vs. specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,7 @@ end
 def stub_berkshelf_api_command
   # Stubbing a command that's executed in the Berkshelf API cookbook
   stub_command(
-    'cd /opt/berkshelf-api/v1.4.0 && /opt/chef/embedded/bin/bundle check'
+    'cd /opt/berkshelf-api/v2.1.1 && /opt/chef/embedded/bin/bundle check'
   ).and_return(
     'The Gemfile\'s dependencies are satisfied'
   )
